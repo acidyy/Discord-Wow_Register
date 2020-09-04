27c5982c6dfb701d5966cd5699eaee5e6657217c
iThorgrim <contact@open-wow.fr>
2020-09-04 17:05:01 +0200

    Update Discord-Wow_RegisterConf.lua

--- Discord-Wow_RegisterConf.lua
+++ Discord-Wow_RegisterConf.lua
@@ -1,10 +1,29 @@
-local BotConf = {
+local botConf = {
 
   ['user'] = 'wowservers',
   ['password'] = 'wowservers',
   ['authDB'] = 'R0_Auth',
 
-  ['BotToken'] = 'YOUR TOKEN HERE'
+  ['header'] = 'Account create Panel',
+  ['body'] = 'You have made a request to create an account.\n\nPlease send me your account name and password.\n\n__The format to be respected is the following:__\n- AccountName Password\n\nAccount name and password must be separated by a space.\n\n__Exemple:__\n**iThorgrim** **iPassword**',
+  ['img'] = 'https://cdn.discordapp.com/avatars/741549380213014528/d01b405680a7362fa13cefc27552e77b.png',
+  ['color'] = '114, 137, 218',
+
+  ['failed'] = {
+    ['header'] = '__ERROR :__ Username already exist',
+    ['body'] = 'Please choose another username',
+    ['img'] = 'https://cdn.discordapp.com/avatars/741549380213014528/d01b405680a7362fa13cefc27552e77b.png',
+    ['color'] = '255, 0, 0',
+  },
+
+  ['success'] = {
+    ['header'] = '__SUCCESS :__ Your account is now create!',
+    ['body'] = '**__For more security__** please delete message with your username & password !',
+    ['img'] = 'https://cdn.discordapp.com/avatars/741549380213014528/d01b405680a7362fa13cefc27552e77b.png',
+    ['color'] = '0, 255, 0',
+  },
+
+  ['botToken'] = 'NzUxNDA2MzAwNzU1MTMyNTQ4.X1Inpg.GIL34a7GbB6X1Xl1AhKtP5LK8Ww',
 };
 
-return BotConf;
+return botConf;
